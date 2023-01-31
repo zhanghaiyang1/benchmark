eb94416fd01141d81329380f33b10b1e971a645a
zhanghaiyang <z_haiyang@163.com>
2023-01-31 22:27:33 +0800

    feat:瑞再

--- benchmark/t_RZ_test.go
+++ benchmark/t_RZ_test.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"crypto/des"
-	"crypto/md5"
-	"encoding/hex"
+
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strconv"
-	"strings"
+
 	"testing"
-	"time"
 )
 
 /*
@@ -23,14 +17,42 @@ test
 */
 var (
 	apiRZ = "https://sandbox-gw.insuremo.com.cn"
+	secret = "maMbrpeVWtRbjXFlxSa7ka4qGcI6XqJP"
 )
 
+
 func TestGetLink(t *testing.T) {
-	api := apiRZ
 	/*
 
 		1.将HTTP Body的内容和secret key进行字符串拼接；
 		2.对于拼接后的内容使用MD5算法进行签名运算。
 	*/
-	//
+	linkReq := LinkReq{}
+	linkReq.QuotationNumber = "Q123210321"
+	linkReq.ProductCode = "P123"
+	linkReq.ChannelCode = "C001"
+	linkReq.AgreementCode = "A001"
+	linkReq.AgentCode = "A001"
+	linkReq.InsuranceCompanyCode = "SRSH"
+	linkReq.BusinessType = "Pro"
+	linkReq.BusinessFrom = "Agent"
+	linkReq.ExternalInfo = "{}"
+	linkReq.Factors = "{}"
+
+	//获取sign
+	bodyByte, _ := json.Marshal(linkReq)
+	fmt.Println("bodyByte:", string(bodyByte))
+
+}
+type LinkReq struct {
+	QuotationNumber      string `json:"quotationNumber"`
+	ProductCode          string `json:"productCode"`
+	ChannelCode          string `json:"channelCode"`
+	AgreementCode        string `json:"agreementCode"`
+	AgentCode            string `json:"agentCode"`
+	InsuranceCompanyCode string `json:"insuranceCompanyCode"`
+	BusinessType         string `json:"businessType"`
+	BusinessFrom         string `json:"businessFrom"`
+	ExternalInfo         string `json:"externalInfo"`
+	Factors              string `json:"factors"`
 }
