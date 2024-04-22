15f65b0b605f61975014d4f9cccfad0fac574285
ocean <z_haiyang@163.com>
2024-04-22 10:09:13 +0800

    422

--- benchmark/main.go
+++ benchmark/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-
+	"fmt"
+	"time"
 )
 
-
 func main() {
-	
+	aType := int32(0)
+	go func() {
+		aType = 1
+		fmt.Printf("1执行时间：%d", time.Now().Unix())
+	}()
+	time.Sleep(time.Second * 5)
+	go func() {
+		fmt.Printf("2执行时间：%d", time.Now().Unix())
+	}()
+	fmt.Println("atype:", aType)
 }
